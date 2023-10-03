453f3e2ba0c2a9008022e1cd16c39dd6dfb7abfd
Thien Vu <60612869+tvudev@users.noreply.github.com>
2023-10-03 16:44:42 +0700

    Create link-preload.js

---
+++ link-preload.js
@@ -0,0 +1,21 @@
+if (id == '001') {
+
+  get_link = 'google.com';
+
+} else if (id == '002') {
+
+  get_link = 'toolshay.com';
+
+} else if (id == '003') {
+
+  get_link = 'link3';
+
+} else if (id == 'n') {
+
+  get_link = 'linkn';
+
+} else {
+
+  get_link = '/';
+
+}
